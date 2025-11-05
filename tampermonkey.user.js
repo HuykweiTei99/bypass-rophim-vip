12d59dffad63b33cbb0d236f9bb4050b59024752
Phạm Ngọc Huy <121278902+HuykweiTei99@users.noreply.github.com>
2025-11-05 19:03:50 +0700

    .

--- tampermonkey.user.js
+++ tampermonkey.user.js
@@ -1,13 +1,13 @@
 // ==UserScript==
-// @name         Rophim Full VIP
+// @name         Rophim Full VIP 2
 // @namespace    http://tampermonkey.net/
 // @version      1.2
 // @description  Bypass VIP + Coin trên Rophim
 // @author       FireT
-// @require      https://raw.githubusercontent.com/firetofficial/bypass-rophim-vip/refs/heads/main/rophim_vip.js
-// @homepageURL   https://github.com/firetofficial/bypass-rophim-vip
-// @downloadURL   https://raw.githubusercontent.com/firetofficial/bypass-rophim-vip/refs/heads/main/tampermonkey.user.js
-// @updateURL     https://raw.githubusercontent.com/firetofficial/bypass-rophim-vip/refs/heads/main/tampermonkey.user.js
+// @require      https://raw.githubusercontent.com/HuykweiTei99/bypass-rophim-vip/refs/heads/main/rophim_vip.js
+// @homepageURL   https://github.com/HuykweiTei99/bypass-rophim-vip
+// @downloadURL   https://raw.githubusercontent.com/HuykweiTei99/bypass-rophim-vip/refs/heads/main/tampermonkey.user.js
+// @updateURL     https://raw.githubusercontent.com/HuykweiTei99/bypass-rophim-vip/refs/heads/main/tampermonkey.user.js
 // @match        *://www.rophim.me/*
 // @match        *://www.rophim.mx/*
 // @match        *://www.rophim.li/*
